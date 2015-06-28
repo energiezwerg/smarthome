d4f4e3d6f0448687137da027e8d0dca01af6c637
Oliver Hinckel <info@ollisnet.de>
2015-06-28 16:20:32 +0200

    - add special handling for dict and list when matching attribute values in   match_items()

--- bin/smarthome.py
+++ bin/smarthome.py
@@ -416,7 +416,7 @@ class SmartHome():
         attr, __, val = attr.partition('[')
         val = val.rstrip(']')
         if attr != '' and val != '':
-            return [self.__item_dict[item] for item in self.__items if regex.match(item) and attr in self.__item_dict[item].conf and val == self.__item_dict[item].conf[attr]]
+            return [self.__item_dict[item] for item in self.__items if regex.match(item) and attr in self.__item_dict[item].conf and ((type(self.__item_dict[item].conf[attr]) in [list,dict] and val in self.__item_dict[item].conf[attr]) or (val == self.__item_dict[item].conf[attr]))]
         elif attr != '':
             return [self.__item_dict[item] for item in self.__items if regex.match(item) and attr in self.__item_dict[item].conf]
         else:
