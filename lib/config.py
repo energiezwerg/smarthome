92cbc75d25876bbcd126422325f7d391256bb5b1
Bernd Meiners <marook@gmx.net>
2016-07-11 21:00:56 +0200

    fixed digit at start of item or attrib name

--- lib/config.py
+++ lib/config.py
@@ -2,21 +2,22 @@
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 #########################################################################
 # Copyright 2013 Marcus Popp                               marcus@popp.mx
+# Copyright 2016 The SmartHomeNG team
 #########################################################################
-#  This file is part of SmartHome.py.    http://mknx.github.io/smarthome/
+#  This file is part of SmartHomeNG.
 #
-#  SmartHome.py is free software: you can redistribute it and/or modify
+#  SmartHomeNG is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  SmartHome.py is distributed in the hope that it will be useful,
+#  SmartHomeNG is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with SmartHome.py. If not, see <http://www.gnu.org/licenses/>.
+#  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
 import logging
@@ -27,16 +28,46 @@ logger = logging.getLogger(__name__)
 
 def strip_quotes(string):
     string = string.strip()
-    if string[0] in ['"', "'"]:  # check if string starts with ' or "
-        if string[0] == string[-1]:  # and end with it
-            if string.count(string[0]) == 2:  # if they are the only one
-                string = string[1:-1]  # remove them
+    if len(string) > 0:
+        if string[0] in ['"', "'"]:  # check if string starts with ' or "
+            if string[0] == string[-1]:  # and end with it
+                if string.count(string[0]) == 2:  # if they are the only one
+                    string = string[1:-1]  # remove them
     return string
 
 
 def parse(filename, config=None):
-    valid_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_@*'
-    valid_set = set(valid_chars)
+    """
+    this functions parses a file with a given filename for config entries
+    
+    :param filename: the filename of the file that provides the configuration entries
+    :param config: already existing config information, should be an ordered dict
+    
+    The config file should stick to the following setup:
+    [firstlevel]
+        attribute1 = xyz
+        attribute2 = foo
+        attribute3 = bar
+        [[secondlevel]]
+            attribute1 = abc
+            attribute2 = bar
+            attribute3 = foo
+            [[[thirdlevel]]]
+                attribute1 = def
+                attribute2 = barfoo
+                attribute3 = foobar
+        [[anothersecondlevel]]
+            attribute1 = andsoon
+            
+    where firstlevel, secondlevel, thirdlevel and anothersecondlevel are defined as items and attribute are their respective attribute - value pairs
+    
+    Valid characters for the items are a-z and A-Z plus any digit and underscore as second or further characters.
+    Valid characters for the attributes are the same as for an item plus @ and *
+    """
+    valid_item_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
+    valid_attr_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_@*'
+    digits = '0123456789'
+    valid_set = set(valid_attr_chars)
     if config is None:
         config = collections.OrderedDict()
     item = config
@@ -62,14 +93,23 @@ def parse(filename, config=None):
                         brackets -= 1
                     else:
                         closing = True
-                        if line[index] not in valid_chars + "'":
-                            logger.error("Problem parsing '{}' invalid character in line {}: {}. Valid characters are: {}".format(filename, linenu, line, valid_chars))
+                        if line[index] not in valid_item_chars + "'":
+                            logger.error("Problem parsing '{}' invalid character in line {}: {}. Valid characters are: {}".format(filename, linenu, line, valid_item_chars))
                             return config
                 if brackets != 0:
                     logger.error("Problem parsing '{}' unbalanced brackets in line {}: {}".format(filename, linenu, line))
                     return config
                 name = line.strip("[]")
                 name = strip_quotes(name)
+                
+                if len(name) > 0:
+                    if name[0] in digits:
+                        logger.error("Problem parsing '{}': item starts with digit '{}' in line {}: {}".format(filename, name[0], linenu, line))
+                        return config
+                else:
+                    logger.error("Problem parsing '{}' tried to use an empty item name in line {}: {}".format(filename, linenu, line))
+                    return config
+                    
                 if level == 1:
                     if name not in config:
                         config[name] = collections.OrderedDict()
@@ -92,8 +132,12 @@ def parse(filename, config=None):
                     continue
                 attr = attr.strip()
                 if not set(attr).issubset(valid_set):
-                    logger.error("Problem parsing '{}' invalid character in line {}: {}. Valid characters are: {}".format(filename, linenu, attr, valid_chars))
+                    logger.error("Problem parsing '{}' invalid character in line {}: {}. Valid characters are: {}".format(filename, linenu, attr, valid_attr_chars))
                     continue
+                    
+                if len(attr) > 0:
+                    if attr[0] in digits:
+                        logger.error("Problem parsing '{}' attrib starts with a digit '{}' in line {}: {}.".format(filename, attr[0], linenu, attr ))
                 if '|' in value:
                     item[attr] = [strip_quotes(x) for x in value.split('|')]
                 else:
