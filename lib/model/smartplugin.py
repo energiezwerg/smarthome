0d49779194dac24668e198989188cbe819daf4de
Marc René Frieß <rene.friess@gmx.de>
2016-12-09 06:55:14 +0100

    added gpl 3 header after confirmation from code owner (cstrassburg)

--- lib/model/smartplugin.py
+++ lib/model/smartplugin.py
@@ -4,7 +4,20 @@
 # Copyright 2016 Christian Strassburg c.strassburg(a)gmx.de
 # 
 #########################################################################
-# Personal and non-commercial use only, redistribution is prohibited.
+#  This file is part of SmartHomeNG
+#
+#  SmartHomeNG is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SmartHomeNG is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SmartHomeNG  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 from lib.model.smartobject import SmartObject
 from lib.utils import Utils
