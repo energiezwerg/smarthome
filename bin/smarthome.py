d13972877a9d0ec96fc3bcf75ba2e322de4ba2ca
cstrassburg <c.strassburg@gmx.de>
2017-02-15 13:46:39 +0100

    version inc

--- bin/smarthome.py
+++ bin/smarthome.py
@@ -83,7 +83,7 @@ from lib.constants import (YAML_FILE, CONF_FILE, DEFAULT_FILE)
 # Globals
 #####################################################################
 MODE = 'default'
-VERSION = '1.3.'
+VERSION = '1.4.'
 TZ = gettz('UTC')
 try:
     os.chdir(BASE)
