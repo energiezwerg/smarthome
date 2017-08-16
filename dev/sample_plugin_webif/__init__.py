b376d59af00aa34eb2335747b32ba519af92b274
msinn <info1@msinn.de>
2017-08-16 17:50:07 +0200

    Added sample plugin with web interface to /dev folder

---
+++ dev/sample_plugin_webif/__init__.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+# vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
+#########################################################################
+#  Copyright 2017 <AUTHOR>                                        <EMAIL>
+#########################################################################
+#  This file is part of SmartHomeNG.   
+#
+#  Sample plugin for new plugins to run with SmartHomeNG version 1.4 and
+#  upwards.
+#
+#  SmartHomeNG is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SmartHomeNG is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SmartHomeNG. If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################
+
+from lib.model.smartplugin import SmartPlugin
+
+
+class SamplePlugin(SmartPlugin):
+    """
+    Main class of the Plugin. Does all plugin specific stuff and provides
+    the update functions for the items
+    """
+    
+    ALLOW_MULTIINSTANCE = False
+    PLUGIN_VERSION='1.3a.0'
+
+
+    def __init__(self, sh, *args, **kwargs):
+        """
+        Initalizes the plugin. The parameters describe for this method are pulled from the entry in plugin.conf.
+
+        :param sh:  **Deprecated**: The instance of the smarthome object. For SmartHomeNG versions **beyond** 1.3: **Don't use it**! Use the method self.get_sh() instead
+        """
+        # attention:
+        # if your plugin runs standalone, sh will likely be None so do not rely on it later or check it within your code
+        
+
+        # Initialization code goes here
+
+        self.init_webinterface()
+
+
+    def run(self):
+        """
+        Run method for the plugin
+        """        
+        self.logger.debug("Plugin '{}': run method called".format(self.get_shortname()))
+        self.alive = True
+        # if you want to create child threads, do not make them daemon = True!
+        # They will not shutdown properly. (It's a python bug)
+
+
+    def stop(self):
+        """
+        Stop method for the plugin
+        """
+        self.logger.debug("Plugin '{}': stop method called".format(self.get_shortname()))
+        self.alive = False
+
+
+    def parse_item(self, item):
+        """
+        Default plugin parse_item method. Is called when the plugin is initialized.
+        The plugin can, corresponding to its attribute keywords, decide what to do with
+        the item in future, like adding it to an internal array for future reference
+        :param item:    The item to process.
+        :return:        If the plugin needs to be informed of an items change you should return a call back function
+                        like the function update_item down below. An example when this is needed is the knx plugin
+                        where parse_item returns the update_item function when the attribute knx_send is found.
+                        This means that when the items value is about to be updated, the call back function is called
+                        with the item, caller, source and dest as arguments and in case of the knx plugin the value
+                        can be sent to the knx with a knx write function within the knx plugin.
+        """
+        if self.has_iattr(item.conf, 'foo_itemtag'):
+            self.logger.debug("Plugin '{}': parse item: {}".format(self.get_shortname(), item))
+
+        # todo
+        # if interesting item for sending values:
+        #   return update_item
+
+
+    def parse_logic(self, logic):
+        """
+        Default plugin parse_logic method
+        """
+        if 'xxx' in logic.conf:
+            # self.function(logic['name'])
+            pass
+
+
+    def update_item(self, item, caller=None, source=None, dest=None):
+        """
+        Write items values
+        :param item: item to be updated towards the plugin
+        :param caller: if given it represents the callers name
+        :param source: if given it represents the source
+        :param dest: if given it represents the dest
+        """
+        # todo 
+        # change 'foo_itemtag' into your attribute name
+        if item():
+            if self.has_iattr(item.conf, 'foo_itemtag'):
+                self.logger.debug("Plugin '{}': update_item ws called with item '{}' from caller '{}', source '{}' and dest '{}'".format(self.get_shortname(), item, caller, source, dest))
+            pass
+
+        # PLEASE CHECK CODE HERE. The following was in the old skeleton.py and seems not to be 
+        # valid any more 
+        # # todo here: change 'plugin' to the plugin name
+        # if caller != 'plugin':  
+        #    logger.info("update item: {0}".format(item.id()))
+
+
+    def init_webinterface(self):
+        """"
+        Initialize the web interface for this plugin
+
+        This method is only needed if the plugin is implementing a web interface
+        """
+        self.mod_http = self.get_module('http')
+        if self.mod_http == None:
+            self.logger.error("Plugin '{}': Not initializing the web interface".format(self.get_shortname()))
+            return
+        
+        # set application configuration for cherrypy
+        webif_dir = self.path_join(self.get_plugin_dir(), 'webif')
+        config = {
+            '/': {
+                'tools.staticdir.root': webif_dir,
+            },
+            '/static': {
+                'tools.staticdir.on': True,
+                'tools.staticdir.dir': 'static'
+            }
+        }
+        
+        # Register the web interface as a cherrypy app
+        self.mod_http.register_app(WebInterface(webif_dir, self), 
+                                   self.get_shortname(), 
+                                   config, 
+                                   self.get_classname(), self.get_instance_name(),
+                                   description='')
+                                   
+        return True
+
+
+# ------------------------------------------
+#    Webinterface of the plugin
+# ------------------------------------------
+
+import cherrypy
+from jinja2 import Environment, FileSystemLoader
+
+class WebInterface:
+
+
+    def __init__(self, webif_dir, plugin):
+        """
+        Initialization of instance of class WebInterface
+        
+        :param webif_dir: directory where the webinterface of the plugin resides
+        :param plugin: instance of the plugin
+        :type webif_dir: str
+        :type plugin: object
+        """
+        self.webif_dir = webif_dir
+        self.plugin = plugin
+        self.tplenv = Environment(loader=FileSystemLoader(self.plugin.path_join( self.webif_dir, 'templates' ) ))
+
+
+    @cherrypy.expose
+    def index(self):
+        """
+        Build index.html for cherrypy
+        
+        Render the template and return the html file to be delivered to the browser
+            
+        :return: contents of the template after beeing rendered 
+        """
+        tmpl = self.tplenv.get_template('index.html')
+        return tmpl.render(plugin_shortname=self.plugin.get_shortname(), plugin_version=self.plugin.get_version(), plugin_info=self.plugin.get_info())
+
