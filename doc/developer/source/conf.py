a801600847ec48bf410a20c8d7490860edfcbafc
msinn <info1@msinn.de>
2018-05-21 11:23:48 +0200

    Documentation changes

--- doc/developer/source/conf.py
+++ doc/developer/source/conf.py
@@ -141,7 +141,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'collapse_navigation': False,
+    'collapse_navigation': True,
     'display_version': False,
     'navigation_depth': 4,
 }
